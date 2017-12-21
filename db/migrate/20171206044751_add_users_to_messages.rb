a442556f75219e736a6cc5e0a1d83c3eebbc54ca
Sophia-Sergio <san.storres@gmail.com>
2017-12-21 13:06:55 -0300

    asad

--- db/migrate/20171206044751_add_users_to_messages.rb
+++
@@ -1,6 +0,0 @@
-class AddUsersToMessages < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :messages, :user_to, foreign_key: true
-    add_reference :messages, :user_from, foreign_key: true
-  end
-end
